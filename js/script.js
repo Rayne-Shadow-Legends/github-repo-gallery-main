da64c3069e0b8e81629fe7864ba9834d64ee1f6a
ThePenguin <thegoldenbella22@gmail.com>
2021-06-22 23:58:36 -0700

    fetched profile data from github and displayed it on the page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-let profileInfo = document.querySelector(".overview")
+let overview = document.querySelector(".overview")
 let username = "Rayne-Shadow-Legends"
 
 let gitUserInfo = async function() {
-    let userInfo = await fetch(`https://api.github.com/users/${username}`);
-    UserInfo = await userInfo.json;
+    let userInfoRequest = await fetch(`https://api.github.com/users/${username}`);
+    userInfo = await userInfoRequest.json();
     console.log(userInfo);
-}
+    displayUserInfo(userInfo);
+};
 
-gitUserInfo();
+    gitUserInfo();
 
-let gaming;
+    let displayUserInfo = function(){
+        let div = document.createElement("div");
+        div.classList.add("user-info");
+
+        div.innerHTML = `<div> 
+        <figure>
+        <img alt="user avatar" src=${userInfo.avatar_url}/>
+      </figure>
+      <div>
+        <p><strong>Name:</strong> ${userInfo.name}</p>
+        <p><strong>Bio:</strong> ${userInfo.bio}</p>
+        <p><strong>Location:</strong> ${userInfo.location}</p>
+        <p><strong>Number of public repos:</strong> ${userInfo.public_repos}</p>
+      </div>
+        </div>`
+        overview.append(div);
+    };
