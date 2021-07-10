94fa6f159b1c3efc4733eba1e0fcff7663ffd90f
ThePenguin <32444070+Rayne-Shadow-Legends@users.noreply.github.com>
2021-07-09 18:05:05 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let overview = document.querySelector(".overview");
 let username = "Rayne-Shadow-Legends";
 
-let reposList;
+let repoList = document.querySelector(".repo-list");
 
 let gitUserInfo = async function() {
     let userInfoRequest = await fetch(`https://api.github.com/users/${username}`);
@@ -16,23 +16,34 @@ let gitUserInfo = async function() {
         let div = document.createElement("div");
         div.classList.add("user-info");
 
-        div.innerHTML = `<div> 
+        div.innerHTML = `
         <figure>
-        <img alt="user avatar" src=${userInfo.avatar_url}/>
-      </figure>
-      <div>
-        <p><strong>Name:</strong> ${userInfo.name}</p>
-        <p><strong>Bio:</strong> ${userInfo.bio}</p>
-        <p><strong>Location:</strong> ${userInfo.location}</p>
-        <p><strong>Number of public repos:</strong> ${userInfo.public_repos}</p>
-      </div>
-        </div>`;
-        overview.append(div);
+          <img alt="user avatar" src=${userInfo.avatar_url} />
+        </figure>
+        <div>
+          <p><strong>Name:</strong> ${userInfo.name}</p>
+          <p><strong>Bio:</strong> ${userInfo.bio}</p>
+          <p><strong>Location:</strong> ${userInfo.location}</p>
+          <p><strong>Number of public repos:</strong> ${userInfo.public_repos}</p>
+        </div>
+      `;
+      overview.append(div);
     };
 
 let getRepos = async function() {
   let repoRequest = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
   let repos = await repoRequest.json();
   console.log(repos);
+  displayRepoInfo(repos);
 };
 
+let displayRepoInfo = function(repos) {
+  for(let repo of repos) {
+    let li = document.createElement("li");
+    li.classList.add("repo")
+    li.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(li);
+  }
+};
+
+getRepos();
